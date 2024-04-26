b87a0c3f95f7f39c4298c621db7c70167a8fc111
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-04-26 17:49:07 +0500

    fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,12 @@ const App = () => {
 
   return (
     <>
-      <div
-        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-        style={{
-          backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-        }}
-      >
+      <div className="main w-full flex flex-wrap justify-center items-center bg-cover bg-no-repeat p-4">
         <div className="w-full">
           <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-            <h1 className="text-center text-2xl font-bold">Currency Converter</h1>
+            <h1 className="text-center text-2xl font-bold">
+              Currency Converter
+            </h1>
             <form>
               <div className="w-full mb-1">
                 <InputBox
