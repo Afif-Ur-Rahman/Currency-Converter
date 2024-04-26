e7dcd20f3cb406c27701f2e3a1805b5b118f5efd
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-04-26 16:48:42 +0500

    Added Search for Currencies

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import ReactSelect from "react-select";
 
 function InputBox({
   label,
@@ -19,22 +20,23 @@ function InputBox({
           placeholder="Amount"
           value={amount}
           onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
-          disabled= {amountDisable}
+          disabled={amountDisable}
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/70 mb-2 w-full">Currency Type</p>
-        <select
+        <ReactSelect
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-          value={selectCurrency}
-          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-        >
-          {currencyOptions?.map((currency) => (
-            <option key={currency} value={currency}>
-              {currency.toUpperCase()}
-            </option>
-          ))}
-        </select>
+          value={{ value: selectCurrency, label: selectCurrency.toUpperCase() }}
+          options={currencyOptions.map((currency) => ({
+            value: currency,
+            label: currency.toUpperCase(),
+          }))}
+          onChange={(selectedOption) =>
+            onCurrencyChange && onCurrencyChange(selectedOption.value)
+          }
+          isSearchable={true}
+        />
       </div>
     </div>
   );
